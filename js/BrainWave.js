1b5ade88f9fedbba22ea82b5c7d4a052d8383895
makumitatsu <87363108+makumitatsu@users.noreply.github.com>
2025-02-01 01:41:45 +0900

    Add files via upload

---
+++ js/BrainWave.js
@@ -0,0 +1,36 @@
+const sections = [
+	{
+		id: "BrainWave",
+		title: "脳波測定の基礎",
+		steps: [
+			{
+				title: "測定する部位と効果",
+				explanation:
+					"測定する場所によって、脳の働く場所が違います<br>F3,F4は運動野といって、運動に関係する実験で脳波をとる際に使います<br>※どのような実験を行うかで、測定位置は重要です!!!<br>左右の差をとることが大切です。F3,F4どちらも測定してください<br>同じように、C3,C4とかP3,P4のように左右で測定してください",
+				image: "img/BrainWave/process1.png",
+			},
+			{
+				title: "測定部位",
+				explanation:
+					"上記では、部位と効果でしたが、測定部位はこの方法でやります",
+				image: "img/BrainWave/process2.png",
+			},
+			{
+				title: "実験の順序",
+				explanation:
+					"NeoRec起動<br>ワックス、角質落とし、アルコールを用意<br>アルコール、角質落としの順番で塗ってください<br>電極にワックスを付けて、マスキングテープで固定",
+			},
+			{
+				title: "実験の後",
+				explanation:
+					"電極を使ったら、こうやって水につけてください<br>1時間以上浸しても大丈夫<br>電極から勝手にワックスが取れます",
+				image: "img/BrainWave/process3.png",
+			},
+			{
+				title: "電極を干す",
+				explanation: "電極を洗ったら、写真のようにかけてください",
+				image: "img/BrainWave/process4.png",
+			},
+		],
+	},
+];
